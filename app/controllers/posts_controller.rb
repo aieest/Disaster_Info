31512dbc6ca74d088fe90de9ab47a7eab88fd634
August Estidola <aie.estidola@gmail.com>
2023-10-09 16:32:49 +0800

    Finished Posts Feature(not yetr partialed)

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,40 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  def index
+  @posts = Post.all
+  end
+  def new
+    @post = Post.new
+  end
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = 'Post created successfully'
+      redirect_to posts_path
+    else
+      flash.now[:alert] = 'Post create failed'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @post.update(post_params)
+      flash[:notice] = 'Post updated successfully'
+      redirect_to posts_path
+    else
+      flash.now[:alert] = 'Post update failed'
+      render :edit, status: :unprocessable_entity
+    end
+  end
+end
+
+def destroy
+  @post.destroy
+  flash[:notice] = 'Post destroyed successfully'
+  redirect_to posts_path
+end
